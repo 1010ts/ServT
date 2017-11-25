26a2a6db3ed124e75dae407dd769f6bd3f0ba065
Alex <Alex@Surface>
2017-11-25 16:27:42 +0200

    123

---
+++ tryCCC/tryCCC/Controllers/NewOrderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace tryCCC.Controllers
+{
+    public class NewOrderController : ApiController
+    {
+        // POST: api/NewOrder
+        public void Post([FromBody]string value)
+        {
+
+        }
+    }
+}
